228edbd0584d64ded9ed6d9451f146e543857ef7
Moritz Gottschling <moritz.gottschling@tngtech.com>
2024-10-11 10:00:55 +0200

    Format tests

--- tests/end2end_tests.py
+++ tests/end2end_tests.py
@@ -2,19 +2,26 @@ import openai
 from dotenv import load_dotenv
 from os import getenv
 
+from openai.types.chat import ChatCompletionMessageParam
+
 load_dotenv()
 
 openai.base_url = "http://localhost:8000/v1/"
 openai.api_key = getenv("AA_TOKEN")
 
+
 def test_embeddings():
     response = openai.embeddings.create(input="Apple", model="luminous-base")
     print("Embeddings test:", response)
 
+
 def test_chat_completions():
-    messages = [{"role": "user", "content": "Hello, how are you?"}]
-    response = openai.chat.completions.create(messages=messages, model="luminous-base-control")
+    messages: list[ChatCompletionMessageParam] = [{"role": "user", "content": "Hello, how are you?"}]
+    response = openai.chat.completions.create(
+        messages=messages, model="luminous-base-control"
+    )
     print("Chat completions test:", response)
+
 
 def test_completions():
     response = openai.completions.create(
@@ -22,11 +29,12 @@ def test_completions():
         prompt="What's the capital of Germany?",
         n=1,
         temperature=0.6,
-        max_tokens=50
+        max_tokens=50,
     )
     print("Completions test:", response)
 
+
 if __name__ == "__main__":
-    test_embeddings()
-    # test_chat_completions()
-    test_completions()
+    # test_embeddings()
+    test_chat_completions()
+    # test_completions()
